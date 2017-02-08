d856ce902cd5ceec93a3176ece76d664a5499c9e
jk <fallafeljan@users.noreply.github.com>
2017-02-08 15:35:28 +0100

    webgl bootstrap

--- app/index.js
+++ app/index.js
@@ -1,3 +1,10 @@
+import setupGL from './setupGL'
+
 if (process.env.NODE_ENV !== 'production') {
   System.import('../index.html')
 }
+
+window.addEventListener('load', () => {
+  const canvas = document.getElementById('canvas')
+  setupGL(canvas)
+})
---
+++ app/setupGL.js
@@ -0,0 +1,34 @@
+// sample code taken from
+// <https://developer.mozilla.org/de/docs/Web/API/WebGL_API/Tutorial/Einf%C3%BChrung_in_WebGL>
+
+let gl
+
+function initWebGL(canvas) {
+  gl = null
+  
+  try {
+    gl = canvas.getContext("webgl")
+      || canvas.getContext("experimental-webgl")
+  }
+  catch (err) {
+  }
+  
+  // Wenn wir keinen WebGl Kontext haben
+  
+  if (!gl) {
+    console.error('Could not initialize WebGL')
+
+    gl = null
+  }
+}
+
+export default function setupGL(canvas) {
+  initWebGL(canvas)
+  
+  if (gl) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.enable(gl.DEPTH_TEST)
+    gl.depthFunc(gl.LEQUAL)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+  }
+}
