825c8c2e704d8a6b8495b1db3ff685409ec86c9e
jk <fallafeljan@users.noreply.github.com>
2017-02-08 15:28:38 +0100

    tooling ⚒

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const debug = process.env.NODE_ENV !== 'production'
+
+const config = {
+  entry: `${__dirname}/app/index.js`,
+  output: {
+    path: `${__dirname}/public`,
+    filename: debug ? 'bundle.js' : 'bundle.[hash].js'
+  },
+
+  performance: {
+    hints: debug ? false : 'warning'
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.html$/,
+      use: 'html-loader'
+    }]
+  },
+
+  devServer: {
+    contentBase: `${__dirname}/public`,
+    port: 3000
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: !debug,
+      debug
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    }),
+
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: `${__dirname}/index.html`
+    }),
+
+  ]
+}
+
+module.exports = config
