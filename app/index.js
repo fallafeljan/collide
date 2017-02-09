11909f4a6c733ab5e283c88eba8ca6e83d80d442
jk <fallafeljan@users.noreply.github.com>
2017-02-09 12:13:25 +0100

    canvas full dimensions

--- app/index.js
+++ app/index.js
@@ -4,7 +4,27 @@ if (process.env.NODE_ENV !== 'production') {
   System.import('../index.html')
 }
 
+let canvas
+
+function handleResize() {
+  const {
+    width,
+    height,
+    clientWidth: displayWidth,
+    clientHeight: displayHeight
+  } = canvas
+ 
+  if (width !== displayWidth || height != displayHeight) {
+    canvas.width  = displayWidth
+    canvas.height = displayHeight
+  }
+}
+
 window.addEventListener('load', () => {
-  const canvas = document.getElementById('canvas')
+  canvas = document.getElementById('canvas')
+  
+  handleResize()
+  window.addEventListener('resize', () => handleResize())
+
   setupGL(canvas)
 })
